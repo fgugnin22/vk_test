a73613175ba6e462b8c39c39e06e43f6db706aa5
fgugnin22 <fedorgugnin22@gmail.com>
2024-03-28 11:47:41 +0300

    cors, bug fixes on frontend and production configs

--- frontend/src/components/Modal/Modal.tsx
+++ frontend/src/components/Modal/Modal.tsx
@@ -23,10 +23,14 @@ const Modal: React.FC = () => {
     const target = e.target as HTMLInputElement;
 
     setFormState((prev) => ({ ...prev, [target.name]: target.value }));
+
+    target.setCustomValidity("");
   };
 
   const handlePriceInputChange = (e: React.FormEvent<HTMLInputElement>) => {
     const target = e.target as HTMLInputElement;
+
+    target.setCustomValidity("");
 
     const allowedPattern = /^[0-9,.\$]*$/;
 
@@ -83,11 +87,19 @@ const Modal: React.FC = () => {
     }
   };
 
-  const handleInputFocus = (e: React.FocusEvent<HTMLInputElement>) => {
-    e.target.setCustomValidity("");
-  };
+  // const handleInputFocus = (e: React.FocusEvent<HTMLInputElement>) => {};
 
   const handleInputBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    if (e.target.name === "name") {
+      const regex = /\S/;
+
+      if (!regex.test(e.target.value)) {
+        e.target.setCustomValidity(
+          "В названии товара должен содержаться хотя бы один символ, отличный от пробела!"
+        );
+      }
+    }
+
     e.target.reportValidity();
   };
 
@@ -114,61 +126,65 @@ const Modal: React.FC = () => {
           className="modal-close-btn"
           onClick={handleCloseModalButtonClick}
         ></button>
-        <label className="modal-form-label" htmlFor="product_id">
+        <label className="modal-form-label" htmlFor="name">
           <span>Название: </span>
           <input
             className="modal-form-input"
             type="text"
             onChange={handleInputChange}
             onInvalid={handleInputInvalid}
-            onFocus={handleInputFocus}
+            // onFocus={handleInputFocus}
             onBlur={handleInputBlur}
             value={formState.name}
             name="name"
-            pattern="^\s*\S+\s*$"
+            id="name"
             required
           />
         </label>
-        <label className="modal-form-label" htmlFor="product_id">
+        <label className="modal-form-label" htmlFor="supplier_email">
           <span>Почта поставщинка: </span>
           <input
             className="modal-form-input"
             type="email"
             onChange={handleInputChange}
             onInvalid={handleInputInvalid}
-            onFocus={handleInputFocus}
+            // onFocus={handleInputFocus}
             onBlur={handleInputBlur}
             value={formState.supplier_email}
             name="supplier_email"
-            pattern="^[\w]{1,}@[\w]{1,}.[a-z]{2,}$"
+            id="supplier_email"
+            pattern="^[\w]{1,}@[\w]{1,}\.[a-z]{2,}$"
             required
           />
         </label>
-        <label className="modal-form-label" htmlFor="product_id">
+        <label className="modal-form-label" htmlFor="count">
           <span>Количество: </span>
           <input
             className="modal-form-input"
             type="number"
             onChange={handleInputChange}
             onInvalid={handleInputInvalid}
-            onFocus={handleInputFocus}
+            // onFocus={handleInputFocus}
             onBlur={handleInputBlur}
             value={formState.count}
             name="count"
+            id="count"
             min={1}
             required
           />
         </label>
-        <label className="modal-form-label" htmlFor="product_id">
+        <label className="modal-form-label" htmlFor="price">
           <span>Стоимость: </span>
           <input
             className="modal-form-input"
             type="text"
             onChange={handlePriceInputChange}
             onInvalid={handleInputInvalid}
-            onFocus={handleInputFocus}
+            // onFocus={handleInputFocus}
             onBlur={handleInputBlur}
             value={formState.price}
+            name="price"
+            id="price"
             pattern="^\$\d{1,3}(,\d{3})*(\.\d{2})?$"
             required
           />
